6e16b090e2893ea5a149cf4f4118a7516a14e7e4
Stephen <stephen@whitebolt.net>
2016-07-15 12:23:20 +0100

    Add foundation to admin template and bootstrap it
    Still nothing to see yet.

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,13 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const cleanCSS = require('gulp-clean-css');
 const gutil = require('gulp-util');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const babel = require("gulp-babel");
+
+const scripts = [
+	'./src/scripts/bootstrap.js'
+];
 
 gulp.task('sass', () => {
 	gulp.src(['./src/scss/*.scss'])
@@ -17,4 +24,16 @@ gulp.task('sass', () => {
 			restructuring: true
 		}))
 		.pipe(gulp.dest('./public/css'))
+});
+
+gulp.task('minify', () => {
+	gulp.src(scripts)
+		.pipe(concat('admin.js'))
+		.pipe(gulp.dest('./public/scripts/'))
+		.on('end', () => gulp.src(scripts)
+			.pipe(concat('admin.min.js'))
+			.pipe(babel())
+			.pipe(uglify().on('error', gutil.log))
+			.pipe(gulp.dest('./public/scripts/'))
+		)
 });
