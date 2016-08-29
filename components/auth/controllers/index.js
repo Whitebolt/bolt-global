208d5f1d40d3b64ffdafefc6c7c441a30b0dd7f4
Stephen <stephen@whitebolt.net>
2016-08-29 09:34:36 +0100

    Fixed login
    For some reason this no-longer worker (perhaps it never worked?) It was not logging-out out when password failed, in fact it was reporting failure no-matter what happened but still logging in.

--- components/auth/controllers/index.js
+++ components/auth/controllers/index.js
@@ -8,70 +8,80 @@ const compare = Promise.promisify(bcrypt.compare);
 const hash = Promise.promisify(bcrypt.hash);
 const SALT_WORK_FACTOR = 10;
 
+function loginView(component) {
+	if (component.view) {
+		let doc = component.doc || component.req.doc || {};
+		return component.view(doc._view || "auth/login", doc, component.req, component.parent);
+	}
+
+	return component;
+}
+
+function login(component) {
+	return new Promise(resolve=>{
+		passport.authenticate('local')(component.req, {end:()=>{
+			component.redirect = '/auth/login?authFailed=1';
+			component.req.logout();
+			return resolve(component);
+		}}, ()=>{
+			component.redirect = '/';
+			return resolve(component);
+		});
+	});
+}
+
+function logout(component) {
+	component.req.logout();
+	component.redirect = '/';
+	return Promise.resolve(component);
+}
+
 let exported = {
-  login: function(component) {
-    let req = component.req;
+	login: function(component) {
+		return ((component.req.method === 'GET') ?
+			loginView(component) :
+			((component.req.method === 'POST') ? login(component) : component)
+		);
+	},
 
-    if (req.method === 'GET') {
-      if (component.view) {
-        let doc = component.doc || component.req.doc || {};
-        return component.view(doc._view || "auth/login", doc, component.req, component.parent);
-      }
-    } else if (req.method === 'POST') {
-      return new Promise(resolve => {
-        passport.authenticate('local')(req, {end:()=>{}}, authenticated=>{
-          component.redirect = authenticated ? '/' : '/auth/login?authFailed=1';
-          return resolve(component);
-        });
-      });
-    }
+	logout: logout,
 
-    return Promise.resolve(component);
-  },
+	"change-password": function(component) {
+		let req = component.req;
 
-  logout: function(component) {
-    let req = component.req;
-    req.logout();
-    component.redirect = '/';
-    return Promise.resolve(component);
-  },
+		if (req.method === 'GET') {
+			if (component.view) {
+				let doc = component.doc || component.req.doc || {};
+				return component.view(doc._view || "auth/change-password", doc, component.req, component.parent);
+			}
+		} else if (req.method === 'POST') {
+			return new Promise(resolve => {
+				resolve(compare(req.body.password, req.session.password).then(authenticated => {
+					if (!authenticated) {
+						component.redirect = '/auth/change-password?authFailed=1';
+						return component;
+					}
 
-  "change-password": function(component) {
-    let req = component.req;
+					if (req.body.password1 === req.body.password2) {
+						return genSalt(SALT_WORK_FACTOR)
+							.then(salt => hash(req.body.password1, salt))
+							.then(hashedPassword => {
+								return req.app.db.collection('users').update({
+									_id: bolt.mongoId(req.session._id)
+								}, {
+									$set: {password: hashedPassword}
+								});
+							}).then(user => {
+								component.redirect = '/';
+								return component;
+							});
+					}
+				}));
+			});
+		}
 
-    if (req.method === 'GET') {
-      if (component.view) {
-        let doc = component.doc || component.req.doc || {};
-        return component.view(doc._view || "auth/change-password", doc, component.req, component.parent);
-      }
-    } else if (req.method === 'POST') {
-      return new Promise(resolve => {
-        resolve(compare(req.body.password, req.session.password).then(authenticated => {
-          if (!authenticated) {
-            component.redirect = '/auth/change-password?authFailed=1';
-            return component;
-          }
-
-          if (req.body.password1 === req.body.password2) {
-            return genSalt(SALT_WORK_FACTOR)
-              .then(salt => hash(req.body.password1, salt))
-              .then(hashedPassword => {
-                return req.app.db.collection('users').update({
-                  _id: bolt.mongoId(req.session._id)
-                }, {
-                  $set: {password: hashedPassword}
-                });
-              }).then(user => {
-                component.redirect = '/';
-                return component;
-              });
-          }
-        }));
-      });
-    }
-
-    return Promise.resolve(component);
-  }
+		return Promise.resolve(component);
+	}
 };
 
 module.exports = exported;
