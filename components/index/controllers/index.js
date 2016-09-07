61c0876dbe98bf8e063d61e8d839284ee7c349e9
Stephen <stephen@whitebolt.net>
2016-09-07 22:05:40 +0100

    Small refactor
    Just a style change really, nothing too special.

--- components/index/controllers/index.js
+++ components/index/controllers/index.js
@@ -1,23 +1,25 @@
 'use strict';
 
+
 let exported = {
-  index: function(component) {
-    let req = component.req;
+	index: function(component) {
+		const req = component.req;
+		const app = req.app;
+		const path = bolt.getPathFromRequest(req);
 
-    return bolt.getDoc({
-      query: {path: bolt.getPathFromRequest(req)}, req
-    }).then(doc=>{
-      if (!doc) {
-        throw "Document not found in Database";
-      }
+		return bolt.getDoc({
+			query: {path}, req
+		}).then(doc=>{
+			if (!doc && !app.config.proxy) throw "Document not found in Database";
+			if (doc) {
+				component.template = ((doc.view && req.app.templates[doc.view]) ? doc.view : 'index');
+				req.doc = doc;
+				component.done = true;
+			}
 
-      component.template = ((doc.view && req.app.templates[doc.view]) ? doc.view : 'index');
-      req.doc = doc;
-      component.done = true;
-
-      return component;
-    });
-  }
+			return component;
+		});
+	}
 };
 
 module.exports = exported;
