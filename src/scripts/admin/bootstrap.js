4d5cd3b274b9ca6525610248018ff1358c7d6579
Stephen <stephen@whitebolt.net>
2016-09-09 10:08:49 +0100

    Removed admin code
    This is now in its own project: git@aaron.whitebolt:bolt/admin.git.  Also, reflected change of repository upstream.

--- src/scripts/admin/bootstrap.js
+++
@@ -1,9 +0,0 @@
-angular.element(document).ready(() => {
-	"use strict";
-
-	angular.element(document).foundation();
-	angular.element("[bolt-app]").each((index, appNode) => {
-		var appName = angular.element(appNode).attr("bolt-app");
-		angular.bootstrap(appNode, [appName]);
-	});
-});
