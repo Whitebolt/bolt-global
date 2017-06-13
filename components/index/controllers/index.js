b91349ae24865403ffcfb2830cde3f0c7003603c
Stephen <stephen@whitebolt.net>
2017-06-13 13:02:33 +0100

    Add template backin for json content
    Json content needs calculating from the template and then other data appended to the json object.

--- components/index/controllers/index.js
+++ components/index/controllers/index.js
@@ -1,18 +1,20 @@
 'use strict';
 
 function addTemplate(component, isJson=false) {
-	let template = "index";
+	let template;
 
 	if (isJson) {
 		if (component.req.doc.viewContentOnly && component.req.app.templates[doc.viewContentOnly]) {
 			template = component.req.doc.viewContentOnly
 		} else if (component.req.doc.view && component.req.app.templates[doc.view + "ContentOnly"]) {
 			template = component.req.doc.view + "ContentOnly"
-		} else if (component.req.app.templates[template + "ContentOnly"]) {
-			template = template + "ContentOnly"
+		} else if (component.req.app.templates["indexContentOnly"]) {
+			template = "indexContentOnly"
 		}
 	} else if (component.req.doc.view && component.req.app.templates[doc.view]) {
 		template = component.req.app.templates[doc.view];
+	} else {
+		template = "index";
 	}
 
 	component.template = template;
@@ -42,14 +44,11 @@ let exported = {
 				let jsonExports = (app.components.index.controllers.index._jsonExports || {});
 
 				assignDoc(req, doc);
-				if (isJson && jsonExports.index) {
-					component.data = {};
-					jsonExports.index.forEach(fieldName=>{
-						component.data[fieldName] = req.doc[fieldName];
-					});
-				} else {
-					if (!component.template) addTemplate(component, isJson);
+				if (isJson && jsonExports.index && jsonExports.index.length ) {
+					component.sendFields = bolt.clone(jsonExports.index);
 				}
+
+				if (!component.template) addTemplate(component, isJson);
 
 				if (isJson) {
 					component.mime("json");
