7be7155fbf54064b74710c04510af37c9b5c39d7
Stephen <stephen@whitebolt.net>
2017-09-01 08:29:31 +0100

    Create an upload receiver
    Still needs a lot of work to make more secure but we have here a basic uploader that works.

--- components/upload/controllers/index.js
+++ components/upload/controllers/index.js
@@ -2,13 +2,55 @@
 
 const fs = require('fs');
 
-let exported = {
-	index: function(component, req, done) {
-		let length = Object.keys(req.body).length;
-		let buf = Buffer.alloc(length);
-		for (let n=0; n<length; n++) buf[n] = req.body[n];
+function getBuffer(chunk) {
+	let length = Object.keys(chunk).length;
+	let buf = Buffer.alloc(length);
+	for (let n=0; n<length; n++) buf[n] = chunk[n];
+	return buf;
+}
 
-		fs.writeFile('/var/www/prayer-assistant/website/public/test.png', buf);
+function emitReady(app, req, sizeToDo) {
+	if ((sizeToDo <= 0)  && (app.uploadTracking.size(req.messageId) === 0)) {
+		app.uploadTracking.emit(req.messageId, 'done');
+	} else {
+		app.uploadTracking.emit(req.messageId, 'ready');
+	}
+}
+
+let exported = {
+	index: function(component, req, done, app) {
+		let sizeToDo = req.fileSize;
+		let fileName = '/var/www/prayer-assistant/website/public/' + req.fileName;
+		let ready = false;
+		let count = 0;
+
+		console.log(req.sessionID);
+
+		function append(chunk) {
+			ready = false;
+			count++;
+			let buf = getBuffer(chunk);
+			sizeToDo -= buf.length;
+			fs.appendFile(fileName, buf, (err, result)=>{
+				ready = true;
+				emitReady(app, req, sizeToDo);
+			});
+		}
+
+		function destroy() {
+			app.uploadTracking.clear(req.messageId);
+			app.uploadTracking.removeAllListeners(req.messageId);
+			req = undefined;
+		}
+
+		append(req.body);
+		app.uploadTracking.on(req.messageId, eventType=>{
+			if (((eventType === 'ready') || (eventType === 'chunk')) && ready) {
+				let chunk = app.uploadTracking.next(req.messageId);
+				if (chunk) append(chunk);
+			} else if (eventType === 'done') destroy();
+		});
+
 		done();
 	}
 };
